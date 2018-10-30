6535f506985bfbead4dc083619ef682234f29f9d
“Hugo <hugohoffmann04@gmail.com>
2018-10-30 18:22:34 -0300

    Adicionado material de lista de exercicios 2 - exe01

---
+++ material/exercicios02/exe01.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+1. Realize a definição das constantes e seus valores e faça sua impressão:
+MAIOR_IDADE: 18;
+PERC_ICMS: 25;
+TEXTO_BOASVINDAS: Seja Bem Vindo ao nosso site.
+*/
+
+/*define('MAIOR_IDADE', '18');
+define('PERCENTUAL_ICMS', '25');
+define('TEXTO_BOAS_VINDAS', 'Seja bem vinda ao nosso site');*/
+
+const MAIOR_IDADE = 18;
+const PERCENTUAL_ICMS = 25;
+const TEXTO_BOAS_VINDAS = 'Seja bem vinda ao nosso site';
+
+echo MAIOR_IDADE .'<br>';
+echo PERCENTUAL_ICMS.'<br>';
+echo TEXTO_BOAS_VINDAS.'<br>';
+?>
+
+<br>
+<a href="index.html">Voltar</a>
